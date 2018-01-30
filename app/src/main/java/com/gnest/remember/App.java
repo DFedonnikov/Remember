f04bd597d5d498a9061306fe4720a010399ced7b
Dmitry Fedonnikov <d.fedonnikov@gmail.com>
2018-01-30 12:29:29 +0300

    Added constants and their init for sound, vibration and font pick feature

--- app/src/main/java/com/gnest/remember/App.java
+++ app/src/main/java/com/gnest/remember/App.java
@@ -2,7 +2,11 @@ package com.gnest.remember;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.net.Uri;
+import android.provider.Settings;
+import android.support.v7.preference.PreferenceManager;
 
 import com.gnest.remember.model.db.data.MemoRealmFields;
 import com.gnest.remember.model.db.migration.RealmMigration;
@@ -15,10 +19,17 @@ import io.realm.RealmConfiguration;
 
 public class App extends Application {
 
+    public static final String FONT_PATH = "fonts/CaviarDreams.ttf";
+    public static final String FONT_SIZE_DEFAULT = "16";
+    public static final String FONT_SIZE_KEY = "font_size";
+    public static final String NOTIFICATION_SOUND_KEY = "notification_sound";
+    public static Typeface FONT = Typeface.DEFAULT;
+    public static int FONT_SIZE = 16;
+    public static Uri NOTIFICATION_SOUND = Settings.System.DEFAULT_NOTIFICATION_URI;
+    private static int[] NUM_OF_LINES;
+
     private static App sSelf;
 
-    public static final String FONT_PATH = "fonts/CaviarDreams.ttf";
-    public static Typeface FONT = Typeface.DEFAULT;
 
     public static final Map<String, RealmConfiguration> REALM_CONFIG_MAP = new HashMap<>();
 
@@ -29,6 +40,10 @@ public class App extends Application {
         Realm.init(this);
         configRealm();
         FONT = Typeface.createFromAsset(getAssets(), FONT_PATH);
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        FONT_SIZE = Integer.parseInt(sharedPref.getString(FONT_SIZE_KEY, FONT_SIZE_DEFAULT));
+        NOTIFICATION_SOUND = Uri.parse(sharedPref.getString(NOTIFICATION_SOUND_KEY, Settings.System.DEFAULT_NOTIFICATION_URI.toString()));
+        NUM_OF_LINES = getResources().getIntArray(R.array.numOfLines);
     }
 
     private void configRealm() {
@@ -53,5 +68,17 @@ public class App extends Application {
 
     public static Context self() {
         return sSelf;
+    }
+
+    public static void setFontSize(int fontSize) {
+        FONT_SIZE = fontSize;
+    }
+
+    public static void setNotificationSound(Uri notificationSoundPath) {
+        NOTIFICATION_SOUND = notificationSoundPath;
+    }
+
+    public static int[] getNumOfLines() {
+        return NUM_OF_LINES;
     }
 }
