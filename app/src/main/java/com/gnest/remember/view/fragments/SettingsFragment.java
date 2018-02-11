acc736484a28ce4e2a1fbf1601916444733d435f
Dmitry Fedonnikov <d.fedonnikov@gmail.com>
2018-02-11 16:33:35 +0300

    Fixed navigation logic when returning from settings

--- app/src/main/java/com/gnest/remember/view/fragments/SettingsFragment.java
+++ app/src/main/java/com/gnest/remember/view/fragments/SettingsFragment.java
@@ -9,7 +9,6 @@ import android.os.Bundle;
 import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -175,7 +174,7 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                mListener.onBackButtonPressed();
+                mListener.onBackPressed();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -212,6 +211,6 @@ public class SettingsFragment extends PreferenceFragmentCompat implements Shared
     }
 
     public interface OnSettingsFragmentInteractionListener {
-        void onBackButtonPressed();
+        void onBackPressed();
     }
 }
