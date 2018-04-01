2f68e0371bceda5b410c2ebd54a3d73bc596315b
Dmitry Fedonnikov <d.fedonnikov@gmail.com>
2018-04-01 23:25:08 +0300

    Added notification channel creation for devices running on API 26 and higher

--- app/src/main/java/com/gnest/remember/services/AlarmService.java
+++ app/src/main/java/com/gnest/remember/services/AlarmService.java
@@ -1,13 +1,16 @@
 package com.gnest.remember.services;
 
 import android.app.IntentService;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
 
+import android.support.annotation.RequiresApi;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 
@@ -23,7 +26,7 @@ public class AlarmService extends IntentService {
     public static final String IS_ON_MAIN_SCREEN = "isOnMainScreen";
     public static final long[] VIBRATE_PATTERN = {300, 300, 300, 300};
 
-    public static Intent getServiceIntent(Context context, String notificationText, long savedId, boolean isOnMainScreen) {
+    public static Intent getServiceIntent(Context context, String notificationText, int savedId, boolean isOnMainScreen) {
         Intent intent = new Intent(context, AlarmService.class);
         intent.putExtra(NOTIFICATION_TEXT, notificationText);
         intent.putExtra(NOTIFICATION_MEMO_ID, savedId);
@@ -38,28 +41,45 @@ public class AlarmService extends IntentService {
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         if (intent != null) {
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
                     .setSmallIcon(R.drawable.ic_note)
                     .setContentTitle(getString(R.string.notification_title))
                     .setContentText(intent.getStringExtra(NOTIFICATION_TEXT))
                     .setVibrate(VIBRATE_PATTERN)
                     .setSound(App.NOTIFICATION_SOUND);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                createNotificationChannel(notificationManager);
+                builder.setChannelId(NOTIFICATION_CHANNEL_ID);
+            }
 
             Intent resultIntent = new Intent(this, MainActivity.class);
-            long memoId = intent.getLongExtra(NOTIFICATION_MEMO_ID, -1);
+            int memoId = intent.getIntExtra(NOTIFICATION_MEMO_ID, -1);
             boolean isOnMainScreen = intent.getBooleanExtra(IS_ON_MAIN_SCREEN, true);
             resultIntent.putExtra(NOTIFICATION_MEMO_ID, memoId);
             resultIntent.putExtra(IS_ON_MAIN_SCREEN, isOnMainScreen);
             TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
             stackBuilder.addParentStack(MainActivity.class);
             stackBuilder.addNextIntent(resultIntent);
-            PendingIntent pendingIntent = stackBuilder.getPendingIntent((int) memoId, PendingIntent.FLAG_UPDATE_CURRENT);
+            PendingIntent pendingIntent = stackBuilder.getPendingIntent(memoId, PendingIntent.FLAG_UPDATE_CURRENT);
             builder.setContentIntent(pendingIntent);
-            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-            int notificationId = (int) memoId;
             if (notificationManager != null) {
-                notificationManager.notify(notificationId, builder.build());
+                notificationManager.notify(memoId, builder.build());
             }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotificationChannel(@Nullable NotificationManager notificationManager) {
+        if (notificationManager != null) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            channel.enableVibration(true);
+            channel.setVibrationPattern(VIBRATE_PATTERN);
+            notificationManager.createNotificationChannel(channel);
         }
     }
 
