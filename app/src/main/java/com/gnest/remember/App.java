a9da8d8da4648023fc8eb9947471279ee6e46dcb
Dmitry Fedonnikov <d.fedonnikov@gmail.com>
2018-04-06 21:14:57 +0300

    Added method for creating notification channel

--- app/src/main/java/com/gnest/remember/App.java
+++ app/src/main/java/com/gnest/remember/App.java
@@ -1,11 +1,16 @@
 package com.gnest.remember;
 
 import android.app.Application;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Typeface;
+import android.media.AudioAttributes;
 import android.net.Uri;
+import android.os.Build;
 import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.v7.preference.PreferenceManager;
 
 import com.gnest.remember.model.db.data.MemoRealmFields;
@@ -23,6 +28,8 @@ public class App extends Application {
     public static final String FONT_SIZE_DEFAULT = "16";
     public static final String FONT_SIZE_KEY = "font_size";
     public static final String NOTIFICATION_SOUND_KEY = "notification_sound";
+    public static final String NOTIFICATION_CHANNEL_ID = "com.gnest.remember.NOTIFICATION";
+    public static final long[] VIBRATE_PATTERN = {300, 300, 300, 300};
     public static Typeface FONT = Typeface.DEFAULT;
     public static int FONT_SIZE = 16;
     public static Uri NOTIFICATION_SOUND = Settings.System.DEFAULT_NOTIFICATION_URI;
@@ -42,6 +49,9 @@ public class App extends Application {
         FONT_SIZE = Integer.parseInt(sharedPref.getString(FONT_SIZE_KEY, FONT_SIZE_DEFAULT));
         NOTIFICATION_SOUND = Uri.parse(sharedPref.getString(NOTIFICATION_SOUND_KEY, Settings.System.DEFAULT_NOTIFICATION_URI.toString()));
         NUM_OF_LINES = getResources().getIntArray(R.array.numOfLines);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel();
+        }
     }
 
     private void configRealm() {
@@ -75,6 +85,30 @@ public class App extends Application {
     public static void setNotificationSound(Uri notificationSoundPath) {
         NOTIFICATION_SOUND = notificationSoundPath;
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    private void createNotificationChannel() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        if (notificationManager != null) {
+            NotificationChannel channel = notificationManager.getNotificationChannel(NOTIFICATION_CHANNEL_ID);
+            if (channel == null) {
+                CharSequence name = getString(R.string.channel_name);
+                String description = getString(R.string.channel_description);
+                int importance = NotificationManager.IMPORTANCE_HIGH;
+                AudioAttributes attributes = new AudioAttributes.Builder()
+                        .setUsage(AudioAttributes.USAGE_NOTIFICATION_EVENT)
+                        .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
+                        .build();
+                channel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, name, importance);
+                channel.setDescription(description);
+                channel.setSound(Settings.System.DEFAULT_NOTIFICATION_URI, attributes);
+                channel.enableVibration(true);
+                channel.setVibrationPattern(VIBRATE_PATTERN);
+                notificationManager.createNotificationChannel(channel);
+            }
+        }
+    }
+
 
     public static int[] getNumOfLines() {
         return NUM_OF_LINES;
